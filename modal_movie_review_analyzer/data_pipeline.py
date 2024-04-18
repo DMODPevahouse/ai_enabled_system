b5440a14ec70e8c4a11208bbf8a2c98c3d4ce34a
David Pevahouse <dpevaho1@jh.edu>
2024-04-18 01:10:04 +0000

    modal checkpoint

--- modal_movie_review_analyzer/data_pipeline.py
+++ modal_movie_review_analyzer/data_pipeline.py
@@ -10,8 +10,81 @@ from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-#class ETL_pipeline:
-#    def __init__():
+class ETL_Pipeline:
+    """
+    ETL_Pipeline class for extracting, transforming, and loading data.
+    """
+
+    def __init__(self):
+        """
+        Initializes the ETL_Pipeline class.
+        """
+        self.data = None
+
+    def extract(self, filename):
+        """
+        Extracts data from a .csv file.
+        
+        Parameters:
+        filename (str): The filename of the .csv file.
+        
+        Returns:
+        None
+        """
+        self.data = pd.read_csv(filename)
+
+    def transform(self):
+        """
+        Transforms the extracted data by cleaning, processing, and preparing it for modeling.
+        
+        Returns:
+        None
+        """
+
+        to_export_df = self.data.copy()
+
+        to_export_df.drop(columns='author', inplace=True)
+        to_export_df.drop(columns='bought_together', inplace=True)
+        to_export_df.drop(columns='store', inplace=True)
+        to_export_df.drop(columns='videos', inplace=True)
+        to_export_df.drop(columns='images_x', inplace=True)
+        to_export_df.drop(columns='price', inplace=True)
+        to_export_df.drop(columns='description', inplace=True)
+        to_export_df.drop(columns='features', inplace=True)
+        to_export_df.drop(columns='timestamp', inplace=True)
+        to_export_df.drop(columns='user_id', inplace=True)
+        to_export_df.drop(columns='asin', inplace=True)
+        to_export_df.drop(columns='parent_asin', inplace=True)
+        to_export_df.drop(columns='Unnamed: 0', inplace=True)
+        to_export_df.drop(columns='movie_title', inplace=True)
+        to_export_df.drop(columns='subtitle', inplace=True)
+        to_export_df.drop(columns='images_y', inplace=True)
+        to_export_df.drop(columns='details', inplace=True)
+        to_export_df.drop(columns='categories', inplace=True)
+        to_export_df = to_export_df.set_index('verified_purchase')
+
+        # check if there are any rows with False values in the "verified_purchase" column
+        if False in to_export_df.index:
+            to_export_df = to_export_df.drop(False)
+
+        to_export_df = to_export_df.reset_index(drop=True)
+
+
+        self.data = to_export_df
+        
+
+    def load(self, output_filename):
+        """
+        Saves the transformed data to a .csv file.
+        
+        Parameters:
+        output_filename (str): The filename of the output .csv file.
+        
+        Returns:
+        None
+        """
+        self.data.to_csv(output_filename, index=False)
+        
         
 
 
